31f7fd1bc211d4377f35b75f5f37757e45f2bef3
Liza <lizzzakkka@mail.ru>
2023-10-03 13:24:17 +0300

    changes have been made to the list file

--- list.py
+++ list.py
@@ -1,21 +1,36 @@
 input_str = input("Введите список чисел, разделенных пробелами: ")
-input_list = input_str.split()
+numbers = input_str.split()
 
-def check_number(num_str):
+try:
+    numbers = [int(num) for num in numbers]
+    even_numbers = [num for num in numbers if num % 2 == 0]
+    odd_numbers = [num for num in numbers if num % 2 != 0]
+    negative_numbers = [num for num in numbers if num < 0]
+
+    print("Четные числа:", even_numbers)
+    print("Нечетные числа:", odd_numbers)
+    print("Числа меньше 0:", negative_numbers)
+except ValueError:
+    print("-1")
+
+
+input_str = input("Введите список чисел, разделенных пробелами: ")
+numbers = input_str.split()
+
+def is_integer(s):
     try:
-        num = int(num_str)
-        if num % 2 == 0:
-            return ("Четное", num)
-        else:
-            return ("Нечетное", num)
+        int(s)
+        return True
     except ValueError:
-        return ("Не число", -1)
+        return False
 
-results = list(map(check_number, input_list))
-even_numbers = list(filter(lambda x: x[0] == "Четное", results))
-odd_numbers = list(filter(lambda x: x[0] == "Нечетное", results))
-negative_numbers = list(filter(lambda x: x[1] < 0, results))
+numbers = list(filter(is_integer, numbers))
+numbers = list(map(int, numbers))
 
-print("Четные числа:", [result[1] for result in even_numbers])
-print("Нечетные числа:", [result[1] for result in odd_numbers])
-print("Отрицательные числа:", [result[1] for result in negative_numbers])
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+negative_numbers = list(filter(lambda x: x < 0, numbers))
+
+print("Четные числа:", even_numbers)
+print("Нечетные числа:", odd_numbers)
+print("Числа меньше 0:", negative_numbers)
