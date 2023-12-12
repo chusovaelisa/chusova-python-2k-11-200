cb38834d40cd0128f9ec10cf62547e3b41922463
Liza <lizzzakkka@mail.ru>
2023-12-12 11:06:37 +0300

    fixed link

--- link_normalization_test.py
+++ link_normalization_test.py
@@ -6,7 +6,7 @@ def normalize_image_url(image_url):
     parsed_url = urllib.parse.urlparse(image_url)
 
     if not parsed_url.scheme or not parsed_url.netloc:
-        image_url = "http:" + image_url
+        image_url = urllib.parse.urljoin("http:", image_url)
 
     return image_url
 
@@ -14,7 +14,7 @@ def normalize_image_url(image_url):
 @pytest.mark.parametrize("image_url, expected_url", [
     ("https://http.cat/200.jpg", "https://http.cat/200.jpg"),
     ("//http.cat/200.jpg", "http://http.cat/200.jpg"),
-    ("/200.jpg", "http:/200.jpg")
+    ("/200.jpg", "http://http.cat/200.jpg")
 ])
 def test_normalize_image_url(image_url, expected_url):
     assert normalize_image_url(image_url) == expected_url
